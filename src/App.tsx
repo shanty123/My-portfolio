d12e9bc0d865a8b2dcf7e4a48261acbd0de4e5ab
shanty123 <shantyshabu01@gmail.com>
2025-05-06 11:31:02 +0100

    Updated the comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+
 import Scene from './canvas/Scene'
 import Hero from './sections/Hero'
 import {About} from './sections/About'
@@ -11,28 +11,17 @@ import Projects from './sections/Projects'
 import BottomNav from './components/BottomNav'
 
 function App() {
-  // const [showAbout, setShowAbout] = useState(false)
-
-  // const handleBoxClick = () => {
-  //   console.log("clicked");
-  //   setShowAbout(true)
-  // }
-
+  
   return (
     <>
       <div className="relative w-screen h-screen">
        
-        {/* <Scene onBoxClick={handleBoxClick} /> */}
+       
         <Scene/>
           <Hero />
           <Element name="about">
         <About />
       </Element>
-        {/* {showAbout && (
-          <div className="absolute inset-0 z-50 flex items-center justify-center text-white bg-black">
-            <About />
-          </div>
-        )} */}
          <Element name="skills">
         <Skills />
       </Element>
